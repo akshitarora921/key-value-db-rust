938c8a5b911738d361cfbe50b8b752b39e26c038
Akshit Arora <akshit.arora@gartner.com>
2023-07-08 23:38:53 +0530

    add database

--- src/main.rs
+++ src/main.rs
@@ -15,5 +15,33 @@ fn main() {
         let contents = std::fs::read_to_string("kv.db").expect("oops");
         std::fs::write("kv.db", [contents, content].concat()).unwrap();
     }
-   
+    let database = Database::new().expect("Database creation failed");
+    // let value = database.get(key).unwrap();
+    // println!("value is {}", value)
+}
+
+struct Database {
+    map: HashMap<String, String>,
+}
+
+impl Database {
+    fn new() -> Result<Database, std::io::Error> {
+        let mut map = HashMap::new();
+        let contents = std::fs::read_to_string("kv.db")?;
+        for line in contents.lines() {
+            let mut pair = line.splitn(2, "\t");
+            let key = pair.next().expect("msg");
+            let value = pair.next().expect("msg");
+            map.insert(key.to_owned(), value.to_owned());
+        }
+        return Ok(Database { map: map });
+    }
+    fn insert(mut self, key: String, value: String) -> Result<bool, std::io::Error> {
+        self.map.insert(key, value).expect("insertion failed");
+        return Ok(true);
+    }
+    fn get(self, key: String) -> Result<String, std::io::Error> {
+        let value = self.map.get(&key).expect("get failed");
+        return Ok(value.to_owned());
+    }
 }
