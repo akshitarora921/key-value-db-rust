c0e2dde1138b57bd0f164bde0ec0e7d867df1716
Akshit Arora <akshitarora9211@gmail.com>
2023-07-10 10:52:20 +0530

    add get fuctionality and add trait drop to flush the db

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
+use std::collections::HashMap;
+
 fn main() {
-    let mut arguments = std::env::args().skip(1);
-    let key = arguments.next().expect("Key is not present");
-
-    let value = arguments.next().expect("Value is not present");
-    println!("The key is {} and the value is {}", key, value);
-    let content = format!("{}\t{}\n", key, value);
-
-    // check if file exist
-    // if not then create a file
-    // if yes then add data to the same file
-    if !std::path::Path::new("kv.db").exists() {
-        std::fs::write("kv.db", content).unwrap();
-    } else {
-        let contents = std::fs::read_to_string("kv.db").expect("oops");
-        std::fs::write("kv.db", [contents, content].concat()).unwrap();
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let mut database = Database::new().expect("Database creation failed");
+    match args.len() {
+        1 => {
+            let key = &args[0];
+            println!("The key is {} and there is no value", key);
+            let value = database.get(key.clone());
+            match value {
+                Ok(value) => {
+                    println!("The value associated to the key {} is {}", key, value);
+                }
+                Err(e) => {
+                    print!("Error: {}", e)
+                }
+            }
+        }
+        2 => {
+            let key = &args[0];
+            let value = &args[1];
+            println!("The key is {} and value is {}", key, value);
+            database.insert(key.clone(), value.clone());
+            database.insert(key.to_uppercase(), value.clone());
+        }
+        _ => {
+            // show a help message
+            help();
+        }
     }
-    let database = Database::new().expect("Database creation failed");
-    // let value = database.get(key).unwrap();
-    // println!("value is {}", value)
+    // database.flush().unwrap(); // now moved to trait "DROP"
 }
 
 struct Database {
@@ -27,21 +39,45 @@ struct Database {
 impl Database {
     fn new() -> Result<Database, std::io::Error> {
         let mut map = HashMap::new();
-        let contents = std::fs::read_to_string("kv.db")?;
-        for line in contents.lines() {
-            let mut pair = line.splitn(2, "\t");
-            let key = pair.next().expect("msg");
-            let value = pair.next().expect("msg");
-            map.insert(key.to_owned(), value.to_owned());
+        if std::path::Path::new("kv.db").exists() {
+            let contents = std::fs::read_to_string("kv.db")?;
+            for line in contents.lines() {
+                let mut pair = line.splitn(2, "\t");
+                let key = pair.next().expect("msg");
+                let value = pair.next().expect("msg");
+                map.insert(key.to_owned(), value.to_owned());
+            }
         }
-        return Ok(Database { map: map });
+        return Ok(Database { map });
     }
-    fn insert(mut self, key: String, value: String) -> Result<bool, std::io::Error> {
-        self.map.insert(key, value).expect("insertion failed");
-        return Ok(true);
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
-    fn get(self, key: String) -> Result<String, std::io::Error> {
-        let value = self.map.get(&key).expect("get failed");
-        return Ok(value.to_owned());
+
+    fn get(self, key: String) -> std::io::Result<String> {
+        return Ok(self.map.get(&key).expect("get failed").to_string());
     }
+}
+
+impl Drop for Database {
+    fn drop(&mut self) {
+        let mut contents = String::new();
+        for (key, value) in &self.map {
+            contents.push_str(key);
+            contents.push('\t');
+            contents.push_str(value);
+            contents.push('\n');
+        }
+        let _ = std::fs::write("kv.db", contents);
+    }
+}
+
+fn help() {
+    println!(
+        "usage:
+match_args <string>
+    Check whether given string is the answer.
+match_args {{increase|decrease}} <integer>
+    Increase or decrease given integer by one."
+    );
 }
